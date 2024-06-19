3fd260726bdcebd397dde0683db7ef2c781d4f7a
rickysh <rickysh23@gmail.com>
2024-06-19 09:19:47 +0300

    first commit.

---
+++ README.md
@@ -0,0 +1,6 @@
+# (English) Prompt Assistant (Streamlit (Python) + OpenAI API)
+
+### This app rephrases your prompts, corrects misspellings, and fixes typos (using OpenAI's GPT-3.5 model).
+
+### **Note**: use your OWN ```OPENAI_API_KEY``` environment variable. For details, please follow **step 2** in: [https://platform.openai.com/docs/quickstart](https://platform.openai.com/docs/quickstart)
+![Screenshot](screenshot.jpg)
---
+++ prompt_assist.py
@@ -0,0 +1,38 @@
+from openai import OpenAI
+import pyperclip
+import streamlit as st
+
+
+client = OpenAI()
+
+# st.title("English Prompt Assistant")
+
+left_col, right_col = st.columns(2)
+with left_col:
+    st.title("English Prompt Assistant")
+with right_col:
+    st.image("icon.jpg", width=150)
+# st.image("icon.jpg", width=200)
+
+st.text("Write your prompt:")
+# user_prompt = st.text_input("Write your prompt:")
+user_prompt = st.text_input("")
+rephrased_prompt = ""
+if user_prompt:
+    completion = client.chat.completions.create(
+    model="gpt-3.5-turbo-0125",
+    messages=[
+        {"role": "system", "content": "You are an English teacher, skilled in rephrasing and correcting misspellings and typos of students and non-English speakers."},
+        {"role": "user", "content": user_prompt + " ** Please rephrase this text."}
+    ]
+    )
+
+    rephrased_prompt = completion.choices[0].message.content
+
+st.text("")
+st.text("")
+st.text("")
+st.text("Let's write it better:")
+st.text("")
+st.text("")
+st.code(rephrased_prompt, language="markdown")
